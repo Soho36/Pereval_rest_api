8c3c951dc413c9f5e6478b1845e2ea95b2593020
Soho36 <vladimir.sehirev@gmail.com>
2024-09-16 00:17:20 +0300

    testing psycopg

--- db_interaction_class.py
+++ db_interaction_class.py
@@ -1,25 +1,9 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname=pereval user=postgres")
-
-# Open a cursor to perform database operations
+conn = psycopg2.connect("dbname=pereval user=postgres password=admin")
 cur = conn.cursor()
-
-# Execute a command: this creates a new table
 cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-
-# Pass data to fill a query placeholders and let Psycopg perform
-# the correct conversion (no more SQL injections!)
-cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
-
-# Query the database and obtain data as Python objects
-cur.execute("SELECT * FROM test;")
-cur.fetchone()
-(1, 100, "abc'def")
-
-# Make the changes to the database persistent
 conn.commit()
-
-# Close communication with the database
 cur.close()
 conn.close()
+
