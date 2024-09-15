a89aebbe05bf78ada758f1ba62110cec07c04a46
Soho36 <vladimir.sehirev@gmail.com>
2024-09-16 00:40:48 +0300

    database dump push

---
+++ envvar.py
@@ -0,0 +1,9 @@
+import os
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST", "localhost")
+)
